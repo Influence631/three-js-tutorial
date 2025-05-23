6a0493ee9daec12630dd7a6ab6bfd71706874475
Influence631 <kdmitro80@gmail.com>
2025-05-23 22:24:09 +0100

    changes

--- tutorial/src/main.js
+++ tutorial/src/main.js
@@ -1,11 +1,39 @@
 import * as THREE from 'three'
+import "./style.css"
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+
 
 const scene = new THREE.Scene();
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(5, 5), 
-  new THREE.MeshStandardMaterial({
-      color: 'yellow',
-      metalness: 1
-}));
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight , 0.1, 10000);
+camera.position.z = 30;
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("canvas.threejs"),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-const camera = new THREE.PerspectiveCamera(80, window.window)
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 5, 3), 
+  new THREE.MeshStandardMaterial({
+      color: 0xadfad,
+      metalness: 0.7,
+  })
+);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+ambientLight.intensity = 2;
+scene.add(cube, ambientLight);
+
+
+console.log(cube);
+console.log(scene);
+const controller = new OrbitControls(camera, renderer.domElement)
+function animate(){
+  requestAnimationFrame(animate);
+
+  cube.rotateX(0.01);
+  cube.rotateY(0.01);
+  renderer.render(scene, camera);
+}
+
+animate();
